7c4582a5893d28e4e6c980a8cc8e70e08260933d
黄信程 <huangxincheng@xmles.cn>
2019-03-05 16:26:39 +0800

    git init

---
+++ order-service/src/main/java/com/hxc/cloud/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package com.hxc.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: huangxincheng
+ * <p>
+ * <p>
+ **/
+@SpringBootApplication
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
---
+++ order-service/src/main/java/com/hxc/cloud/order/domain/ProductOrder.java
@@ -0,0 +1,28 @@
+package com.hxc.cloud.order.domain;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Author: huangxincheng
+ * <p>
+ * <p>
+ **/
+@Data
+@Accessors(chain = true)
+public class ProductOrder implements Serializable {
+
+    private Long tradeNo;
+
+    private Date tradeTime;
+
+    private Integer userId;
+
+    private Integer productId;
+
+    private BigDecimal amount;
+}

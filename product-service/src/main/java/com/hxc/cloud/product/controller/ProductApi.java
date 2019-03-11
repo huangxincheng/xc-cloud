9eac6f48bec34e77ce6bb172d3074be4b7daf599
黄信程 <huangxincheng@xmles.cn>
2019-03-11 11:51:21 +0800

    zuul retryable

--- product-service/src/main/java/com/hxc/cloud/product/controller/ProductApi.java
+++ product-service/src/main/java/com/hxc/cloud/product/controller/ProductApi.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Author: huangxincheng
@@ -36,6 +37,11 @@ public class ProductApi {
         response.setProductInfo(product);
         response.setServerPort(serverPort);
         log.info("ProducApi serverPory = {} productInfo = {}", serverPort, product);
+        try {
+            TimeUnit.SECONDS.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return response;
     }
 

1e41664eaf054db7d64940695ed8972c9f4d1e1b
黄信程 <huangxincheng@xmles.cn>
2019-03-14 11:43:09 +0800

    屏蔽Global

--- zuul-api-gateway/src/main/java/com/hxc/cloud/filter/GlobalResponseFilter.java
+++ zuul-api-gateway/src/main/java/com/hxc/cloud/filter/GlobalResponseFilter.java
@@ -1,10 +1,13 @@
 package com.hxc.cloud.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.hxc.cloud.common.response.AppResponse;
 import com.hxc.cloud.filter.constants.IFilterConstants;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
 
@@ -18,7 +21,7 @@ import java.nio.charset.Charset;
  * <p>
  *     全局响应过滤器
  **/
-//@Component
+@Component
 public class GlobalResponseFilter extends ZuulFilter {
     @Override
     public String filterType() {
@@ -40,9 +43,14 @@ public class GlobalResponseFilter extends ZuulFilter {
         RequestContext ctx = RequestContext.getCurrentContext();
         InputStream stream = ctx.getResponseDataStream();
         try {
-            if (stream != null) {
+            if (stream != null && ctx.getResponseStatusCode() == HttpStatus.OK.value()) {
                 String data = StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
-                ctx.setResponseBody("{\"code\":0,\"msg\":\"SUCCESS\",\"data\":"+data+"}");
+                AppResponse appResponse = JSON.parseObject(data, AppResponse.class);
+                if (appResponse.getCode() != null && appResponse.getMsg() != null && appResponse.getData() != null) {
+                    ctx.setResponseBody(data);
+                } else {
+                    ctx.setResponseBody(JSON.toJSONString(AppResponse.ok(data)));
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();

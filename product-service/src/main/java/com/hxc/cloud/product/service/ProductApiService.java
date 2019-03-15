8b57703b2ed3bc6ce79c2e2aa91a707c6839828d
黄信程 <huangxincheng@xmles.cn>
2019-03-15 14:43:56 +0800

    增加部分代码

--- product-service/src/main/java/com/hxc/cloud/product/service/ProductApiService.java
+++ product-service/src/main/java/com/hxc/cloud/product/service/ProductApiService.java
@@ -1,5 +1,6 @@
 package com.hxc.cloud.product.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hxc.cloud.common.utils.TransUtil;
 import com.hxc.cloud.module.product.model.ProductInfoVo;
 import com.hxc.cloud.product.entity.ProductInfo;
@@ -22,15 +23,21 @@ public class ProductApiService {
 
     public ProductInfoVo getProduct(Integer id) {
 //        throw new AppProductException("查询商品失败");
-
+//        ProductInfo p = service.getOne(
+//                new QueryWrapper<ProductInfo>().lambda()
+//                        .and(q -> q.eq(ProductInfo::getProductName, "眼镜框"))
+//                        .and(q->q.eq(ProductInfo::getProductMoney, 1000.5))
+//        );
+//        System.out.println(p);
         ProductInfo productInfo = service.getById(id);
-        return TransUtil.trans(productInfo,productInfo1 -> {
+        return TransUtil.trans(productInfo, p -> {
             return new ProductInfoVo()
-                    .setId(productInfo.getId())
-                    .setPrice(productInfo.getProductMoney())
-                    .setProductName(productInfo.getProductName());
+                    .setId(p.getId())
+                    .setPrice(p.getProductMoney())
+                    .setProductName(p.getProductName());
 
         });
+
     }
 
     public List<ProductInfoVo> listProduct() {
@@ -41,6 +48,33 @@ public class ProductApiService {
                     .setPrice(productInfo.getProductMoney())
                     .setProductName(productInfo.getProductName());
         }).collect(Collectors.toList());
+    }
+
+
+    public List<ProductInfoVo> listProductByName(String name) {
+        List<ProductInfo> list = service.list(new QueryWrapper<ProductInfo>().lambda()
+                .and(q -> q.eq(ProductInfo::getProductName, name)));
+        return list.stream().map(productInfo -> {
+            return new ProductInfoVo()
+                    .setId(productInfo.getId())
+                    .setPrice(productInfo.getProductMoney())
+                    .setProductName(productInfo.getProductName());
+        }).collect(Collectors.toList());
+    }
+
+    public ProductInfoVo getProductByName(String name) {
+        ProductInfo productInfo = service.getOne(new QueryWrapper<ProductInfo>()
+                .last("limit 1")
+                .orderByDesc("ctime")
+                .lambda()
+                .and(q -> q.eq(ProductInfo::getProductName, name)));
+        return TransUtil.trans(productInfo ,p -> {
+            return new ProductInfoVo()
+                    .setId(p.getId())
+                    .setPrice(p.getProductMoney())
+                    .setProductName(p.getProductName());
+
+        });
     }
 
 

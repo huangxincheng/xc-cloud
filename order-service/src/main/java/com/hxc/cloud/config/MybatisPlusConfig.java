4dc88c942c93536f29e2eeaefd4395caf22e7662
黄信程 <huangxincheng@xmles.cn>
2019-03-22 17:47:31 +0800

    用户创建和激活

---
+++ order-service/src/main/java/com/hxc/cloud/config/MybatisPlusConfig.java
@@ -0,0 +1,34 @@
+package com.hxc.cloud.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author: huangxincheng
+ * <p>
+ * <p>
+ **/
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 乐观锁插件
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * 逻辑删除插件
+     */
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+
+}

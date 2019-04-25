edaf7343ba698ef5bdd036bc247d9deeae44890e
huangxincheng <huangxincheng@xmiles.cn>
2019-04-25 15:50:46 +0800

    增加NewApiGateway

---
+++ new-api-gateway/src/main/java/com/hxc/cloud/NewApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.hxc.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xulongchao
+ * @cdate 2019/4/2
+ * @email master@xlchao.com
+ */
+@SpringBootApplication
+public class NewApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewApiGatewayApplication.class, args);
+    }
+}
